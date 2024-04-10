4394e9e759cc1da0d5006fcf822860b4d3f71485
Leandro <leandrocosta1998@gmail.com>
2024-04-10 01:26:46 -0300

    Update nome.js: changed function comment and removed unnecessary description from gerarXP function
    The commit updates the "nome.js" file by modifying the comment of the "gerarXP" function and removing the unnecessary description of the function. The variable "nome" remains unchanged. The function "gerarXP" still generates random XP within a specified range.

---
+++ nome.js
@@ -0,0 +1,39 @@
+// Criação da variável
+let nome = "Leandro";
+
+// Função para gerar XP aleatório dentro de um intervalo
+function gerarXP(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Função para determinar o nível com base na XP
+function determinarNivel(XP) {
+    if (XP < 1000) {
+        return "Ferro";
+    } else if (XP <= 2000) {
+        return "Bronze";
+    } else if (XP <= 5000) {
+        return "Prata";
+    } else if (XP <= 7000) {
+        return "Ouro";
+    } else if (XP <= 8000) {
+        return "Platina";
+    } else if (XP <= 9000) {
+        return "Ascendente";
+    } else if (XP <= 10000) {
+        return "Imortal";
+    } else {
+        return "Radiante";
+    }
+}
+
+// Gerar XP aleatório 
+const minXP = 1;
+const maxXP = 250000;
+let XP = gerarXP(minXP, maxXP);
+
+// Determinar o nível
+let nivel = determinarNivel(XP);
+
+// Saída
+console.log(`O Herói de nome ${nome} está no nível ${nivel} com XP de ${XP}`);
